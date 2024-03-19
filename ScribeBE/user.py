91565c201c9de95e2db3a4cdbca2cd7c4734c320
Nithi Pipatkittikul <fordpipatkitkul@gmail.com>
2024-03-19 16:32:31 -0500

    setting backend for user role

---
+++ ScribeBE/user.py
@@ -0,0 +1,39 @@
+from fastapi import APIRouter, HTTPException
+
+
+user_router = APIRouter()
+
+"""
+I still need to setup mongoDB first, but this backend server should work
+
+    ***instantiate  database object from mongoDB***
+    database = I don't know about syntax
+
+**** signup   idea: we need to push our clinet's info to database*****
+@user_router.post("/SignupPage", status_code=201)
+async def create_user(name:str, email_id:str, password:str) -> str:
+    database.append({
+        "Name" : name,
+        "Email_id" : email_id,
+        "Password" : password
+    })
+
+    return("successfuly signup")
+
+**** login    idea: we need to check that what user log-in is matching what we have in database *****
+
+@user_router.post("/loginSignupPage")
+async def login(name:str, email_id:str, password:str) -> str:
+    
+    check = {
+        "Name" : name,
+        "Email_id" : email_id,
+        "Password" : password
+    }
+    
+    for user in database:
+        if user == check:
+            return ("successfuly login")
+    return ("wrong username&password")
+        
+"""
