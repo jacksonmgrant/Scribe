0de178102e2e4a95ae5603b55b837fa213e3013e
Nithi Pipatkittikul <fordpipatkitkul@gmail.com>
2024-03-26 12:26:51 -0500

    clean up code

--- ScribeFE/scribe-fe/src/components/pages/AboutUs.jsx
+++ ScribeFE/scribe-fe/src/components/pages/AboutUs.jsx
@@ -6,31 +6,31 @@ const About = () => {
     return (
       <div className={styles.container}>
         <div className={styles.person}>
-          <img src={suitFord} alt="ford" width="200" height="200" className={styles.pic}/>
-          <div className={styles.text}>
-            <h2> Nithi Pipatkittikul </h2>
-            <p> role : developing frontend</p>
-          </div>
-        </div>
-        <div className={styles.person}>
           <img src={Jacksonpfp} alt="Jackson Grant" width="200" height="200" className={styles.pic}/>
           <div className={styles.text}>
-            <h2> Jackson Grant </h2>
-            <p> role : Does things </p>
+            <h2>Jackson Grant</h2>
+            <p>Project lead, Full stack Developer</p>
           </div>
         </div>
         <div className={styles.person}>
           <img src={suitFord} alt="ford" width="200" height="200" className={styles.pic}/>
           <div className={styles.text}>
-            <h2> ????????? </h2>
-            <p> role : ?????????</p>
+            <h2>Nithi Pipatkittikul</h2>
+            <p>Full stack Developer, Database designer</p>
           </div>
         </div>
         <div className={styles.person}>
           <img src={suitFord} alt="ford" width="200" height="200" className={styles.pic}/>
           <div className={styles.text}>
-            <h2> ????????? </h2>
-            <p> role : ?????????</p>
+            <h2>Kate Doolittle</h2>
+            <p>UI/UX Designer/Frontend Developer</p>
+          </div>
+        </div>
+        <div className={styles.person}>
+          <img src={suitFord} alt="ford" width="200" height="200" className={styles.pic}/>
+          <div className={styles.text}>
+            <h2>Dylan Crooks</h2>
+            <p>Backend developer,Database designer</p>
           </div>
         </div>
       </div>
