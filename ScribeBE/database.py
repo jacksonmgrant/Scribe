5261162e97472dd6b1ae32df9b91acc6fe3e2047
Nithi Pipatkittikul <fordpipatkitkul@gmail.com>
2024-03-28 20:43:01 -0500

    add comment

--- ScribeBE/database.py
+++ ScribeBE/database.py
@@ -4,8 +4,13 @@ from dotenv import load_dotenv
 from motor.motor_asyncio import AsyncIOMotorClient
 
 from models.note_model import DbNote
-
+from models.user_model import DbUser
 async def init_db():
+    # reads the environment variables from the .env file.
     load_dotenv()
+
+    # connect to your mongodb database. Please change to your database
     client = AsyncIOMotorClient(f"mongodb+srv://{os.getenv("DB_USER")}:{os.getenv("DB_PASS")}@scribedb.klymspw.mongodb.net/")
-    await init_beanie(client.ScribeDB, document_models=[DbNote])
+
+    # selecting collections
+    await init_beanie(client.ScribeDB, document_models=[DbNote], document_models=[DbUser])
