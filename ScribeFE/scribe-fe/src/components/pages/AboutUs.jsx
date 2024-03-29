65a07725c0e58189e217cb6b5e0f9e4707b339ef
junkshark8 <152639308+junkshark8@users.noreply.github.com>
2024-03-29 15:12:45 -0500

    Style about page
    updated layout

--- ScribeFE/scribe-fe/src/components/pages/AboutUs.jsx
+++ ScribeFE/scribe-fe/src/components/pages/AboutUs.jsx
@@ -11,28 +11,28 @@ const About = () => {
           <img src={Jacksonpfp} alt="Jackson Grant" width="200" height="200" className={styles.pic}/>
           <div className={styles.text}>
             <h2>Jackson Grant</h2>
-            <p>Project lead, Full stack Developer</p>
+            <p>Project lead, Full Stack Developer</p>
           </div>
         </div>
         <div className={styles.person}>
           <img src={suitFord} alt="ford" width="200" height="200" className={styles.pic}/>
           <div className={styles.text}>
             <h2>Nithi Pipatkittikul</h2>
-            <p>Full stack Developer, Database designer</p>
+            <p>Full Stack Developer, Database Designer</p>
           </div>
         </div>
         <div className={styles.person}>
           <img src={Kate} alt="kate" width="200" height="200" className={styles.pic}/>
           <div className={styles.text}>
             <h2>Kate Doolittle</h2>
-            <p>UI/UX Designer/Frontend Developer</p>
+            <p>UI/UX Designer, Frontend Developer</p>
           </div>
         </div>
         <div className={styles.person}>
           <img src={me} alt="dylan" width="200" height="200" className={styles.pic} style={{backgroundColor: "white"}}/>
           <div className={styles.text}>
             <h2>Dylan Crooks</h2>
-            <p>Backend developer,Database designer</p>
+            <p>Backend Developer, Database Designer</p>
           </div>
         </div>
       </div>
