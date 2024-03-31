cf5aa7dfc44d4fa224bab1346766d7688ae8e95c
Nithi Pipatkittikul <fordpipatkitkul@gmail.com>
2024-03-30 23:20:26 -0500

    clean up code

--- ScribeFE/scribe-fe/src/components/pages/WelcomePage.jsx
+++ ScribeFE/scribe-fe/src/components/pages/WelcomePage.jsx
@@ -1,7 +1,10 @@
+import React from "react";
+
+
 const Welcomepage = () => {
     return(
-        <div>
-            <p>Don't panic this is gonna be like welcome page. If you login and stuff you will see all the upload file page</p>
+        <div className= "" >
+            <p>Don't panic this is gonna be like welcome page. If you login or signup you will see translater page. That page will be access by only login</p>
         </div>
     )
 }
