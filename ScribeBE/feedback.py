9d8a21e46486b43eaf39f61dbe620b28c72b6461
Nithi Pipatkittikul <fordpipatkitkul@gmail.com>
2024-04-02 23:03:52 -0500

    add 400 message in feedback

--- ScribeBE/feedback.py
+++ ScribeBE/feedback.py
@@ -5,6 +5,8 @@ feedback_router = APIRouter()
 
 @feedback_router.post("/", status_code=201)
 async def recieve_feedback(feedback:Feedback)-> dict:
+    if not feedback.text or not feedback.rating:
+        raise HTTPException(status_code=400, detail={"msg": "Plz fill something"})
     new_feedback = DbFeedback(text=feedback.text,rating=feedback.rating)
     await new_feedback.insert()
     return {"msg": "successfully add new feedback"} 
