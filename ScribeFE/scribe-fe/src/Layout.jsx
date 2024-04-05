6c50cefc6ad851f073651c79a587f50f01bb8157
junkshark8 <152639308+junkshark8@users.noreply.github.com>
2024-04-05 17:52:48 -0500

    fix nav links
    changed nav links back to Link tag from anchor tag

--- ScribeFE/scribe-fe/src/Layout.jsx
+++ ScribeFE/scribe-fe/src/Layout.jsx
@@ -1,4 +1,4 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, Link } from "react-router-dom";
 import Logo from "./components/Assets/logo-bw.png";
 import "./styles/layout.css";
 
@@ -13,26 +13,26 @@ const Layout = ({isSignin,signout}) => {
                   <li>
                     <div style={{ display: 'flex', alignItems:  'start' }}>
                       <img alt="Scribe Logo" src={Logo} style={{ height: '1.75rem', marginRight: '0.5rem' }}></img>
-                      <a href="/" style={{ fontWeight: '700' }}>Scribe</a>
+                      <Link to="/" style={{ fontWeight: '700' }}>Scribe</Link>
                     </div>
                   </li>
                   <li>
-                      <a id="aboutus" href="/aboutus">About us</a>
+                      <Link id="aboutus" to="/aboutus">About us</Link>
                   </li>
                   <li>
-                      <a id="formsubmission" href="/formsubmission">Form submission</a>
+                      <Link id="formsubmission" to="/formsubmission">Form submission</Link>
                   </li>
                   { isSignin === false
                   ?( 
                     <li className="push">
-                        <a id="loginSignupPage" href="/loginSignupPage">Log In
+                        <Link id="loginSignupPage" to="/loginSignupPage">Log In
                         <i className="fa-solid fa-right-to-bracket" style={{ marginLeft: '8px' }}></i>
-                        </a>
+                        </Link>
                     </li>
                     )
                   : (
                     <li className="push">
-                        <a href="/loginSignupPage"  onClick={signout}>Log out</a>
+                        <Link to="/loginSignupPage"  onClick={signout}>Log out</Link>
                         <i className="fa-solid fa-right-to-bracket" style={{ marginLeft: '8px' }}></i>
                     </li>
                     )
