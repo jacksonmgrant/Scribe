7f3049e311e28fd3a9be0ac39d4ed3563fb33cf9
junkshark8 <152639308+junkshark8@users.noreply.github.com>
2024-04-06 09:13:22 -0500

    Add Edit Modal
    Modal now pops up on edit. The bug is you can still click on everything outside the modal while its open. Need to solve that

--- ScribeFE/scribe-fe/src/components/note.jsx
+++ ScribeFE/scribe-fe/src/components/note.jsx
@@ -1,21 +1,27 @@
 import React, { useState } from 'react';
 import apiService from '../services/apiService';
+import '../styles/note.css'
 
 export function Note({id: _id, text = "Empty note", time, hasRecording, onEdit, onDelete}) {
   const [noteText, setText] = useState(text);
   const [isEditing, setIsEditing] = useState(false);
+  const [open, setOpen] = React.useState(false);
+
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
 
   function editNote(e) {
     setText(e.target.value);
   }
 
   function handleEditClick() {
-    setIsEditing(true);
+      handleOpen();
   }
 
   async function handleSaveClick() {
     await apiService.updateNote(_id, noteText)
     onEdit(_id, noteText);
+    handleClose();
     setIsEditing(false);
   }
 
@@ -23,25 +29,40 @@ export function Note({id: _id, text = "Empty note", time, hasRecording, onEdit, 
     // This will need to send a delete request to the server
     await apiService.deleteNoteById(_id);
     onDelete(_id);
-  }
+    }
 
   return (
+    <>
     <div className="note">
       <div className="content">
-        {isEditing ? <textarea type="text" className="text" value={noteText} onChange={editNote} rows={noteText.length/100+1} cols={100} />
+        {isEditing ?
+        <textarea type="text" className="text" value={noteText} onChange={editNote} rows={noteText.length/100+1} cols={100} />
           : <textarea type="text" className="text" value={noteText} onChange={editNote} readOnly rows={noteText.length/100+1} cols={100} />}
       </div>
       <div className="actions">
-        {isEditing ? <button className="edit" onClick={handleSaveClick}>Save
-        <i className="fa-solid fa-square-check" style={{ marginLeft: '8px' }}></i>
+        <button className="edit" onClick={handleEditClick}>Edit
+          <i className="fa-solid fa-pen-to-square" style={{ marginLeft: '8px' }}></i>
         </button>
-          : <button className="edit" onClick={handleEditClick}>Edit
-            <i className="fa-solid fa-pen-to-square" style={{ marginLeft: '8px' }}></i>
-            </button>}
         <button className="delete" onClick={handleDeleteClick}>Delete
         <i className="fa-solid fa-trash" style={{ marginLeft: '8px' }}></i>
         </button>
       </div>
      </div>
+     <div className='dialog-container'>
+        <dialog id="dialog"
+          open={open}
+          aria-labelledby="modal-modal-title"
+          aria-describedby="modal-modal-description">
+          <textarea type="text" className="text" value={noteText} onChange={editNote} />
+          <div className="buttons">
+            <button className="cancel" onClick={handleClose}>Cancel
+            </button>
+            <button className="save" onClick={handleSaveClick}>Save
+              <i className="fa-solid fa-square-check" style={{ marginLeft: '8px' }}></i>
+            </button>
+          </div>
+        </dialog>
+        </div>
+      </>
   );
 }
