c662a5c5aec5a420c3e2cb47f989f117ecd4df60
junkshark8 <152639308+junkshark8@users.noreply.github.com>
2024-04-06 20:53:32 -0500

    Add delete modal
    add confirm delete modal, disable clicking outside modal box

--- ScribeFE/scribe-fe/src/components/note.jsx
+++ ScribeFE/scribe-fe/src/components/note.jsx
@@ -4,67 +4,102 @@ import '../styles/note.css'
 
 export function Note({id: _id, text = "Empty note", time, hasRecording, onEdit, onDelete}) {
   const [noteText, setText] = useState(text);
-  const [isEditing, setIsEditing] = useState(false);
-  const [open, setOpen] = React.useState(false);
 
-  const handleOpen = () => setOpen(true);
-  const handleClose = () => setOpen(false);
+  const [editOpen, setEditOpen] = React.useState(false);
+  const [deleteOpen, setDeleteOpen] = React.useState(false);
+
+  const handleEditOpen = () => setEditOpen(true);
+  const handleDeleteOpen = () => setDeleteOpen(true);
 
   function editNote(e) {
     setText(e.target.value);
   }
 
-  function handleEditClick() {
-      handleOpen();
-  }
-
-  async function handleSaveClick() {
-    await apiService.updateNote(_id, noteText)
-    onEdit(_id, noteText);
-    handleClose();
-    setIsEditing(false);
-  }
-
-  async function handleDeleteClick() {
-    // This will need to send a delete request to the server
-    await apiService.deleteNoteById(_id);
-    onDelete(_id);
+  function EditModal() {
+    const handleEditClose = () => setEditOpen(false);
+  
+    async function handleSaveClick() {
+      await apiService.updateNote(_id, noteText)
+      onEdit(_id, noteText);
+      handleEditClose();
     }
 
-  return (
-    <>
-    <div className="note">
-      <div className="content">
-        {isEditing ?
-        <textarea type="text" className="text" value={noteText} onChange={editNote} rows={noteText.length/100+1} cols={100} />
-          : <textarea type="text" className="text" value={noteText} onChange={editNote} readOnly rows={noteText.length/100+1} cols={100} />}
-      </div>
-      <div className="actions">
-        <button className="edit" onClick={handleEditClick}>Edit
-          <i className="fa-solid fa-pen-to-square" style={{ marginLeft: '8px' }}></i>
-        </button>
-        <button className="delete" onClick={handleDeleteClick}>Delete
-        <i className="fa-solid fa-trash" style={{ marginLeft: '8px' }}></i>
-        </button>
-      </div>
-     </div>
-     {/* edit modal */}
-     <div className='dialog-container'>
-        <dialog
-          open={open}
+    return (
+      editOpen ?
+      <div className='dialog-container'> {/* Kate: styled to prevent user from clicking behind modal */}
+        <dialog className="edit-dialog open"
+          open={editOpen}
           aria-labelledby="modal-modal-title"
           aria-describedby="modal-modal-description">
           <h2>Edit Note</h2>
-          <textarea type="text" className="text" value={noteText} onChange={editNote} />
+          <textarea type="text" className="text" value={noteText} onChange={editNote} rows="1"/>
           <div className="buttons">
-            <button className="cancel" onClick={handleClose}>Cancel
+            <button className="cancel" onClick={handleEditClose}>Cancel
             </button>
             <button className="save" onClick={handleSaveClick}>Save
               <i className="fa-solid fa-square-check" style={{ marginLeft: '8px' }}></i>
             </button>
           </div>
         </dialog>
-        </div>
+      </div>
+      : <div></div> //Kate: if the dialog is not open return an empty div, 
+      //without this div-container is visible always and prevents clicking buttons
+      //unsure if there's a better way to do this but it works for now
+    )
+  }
+
+  function DeleteModal() {
+    const handleDeleteClose = () => setDeleteOpen(false);
+  
+    async function handleDeleteClick() {
+      // This will need to send a delete request to the server
+      await apiService.deleteNoteById(_id);
+      onDelete(_id);
+      handleDeleteClose();
+    }
+
+    return (
+      deleteOpen ?
+      <div className='dialog-container'>
+        <dialog className="delete-dialog"
+          open={deleteOpen}
+          aria-labelledby="modal-modal-title"
+          aria-describedby="modal-modal-description">
+          <h2>Delete Note</h2>
+          <div className="delete-message">
+            <h3><i class="fas fa-exclamation-triangle" style={{marginRight: '8px'}}></i>Are you sure?</h3>
+            <p>This action cannot be undone.</p>
+          </div>
+          <div className="buttons">
+            <button className="cancel" onClick={handleDeleteClose}>Cancel
+            </button>
+            <button className="delete" onClick={handleDeleteClick}>Delete
+            <i className="fa-solid fa-trash" style={{ marginLeft: '8px' }}></i>
+            </button>
+          </div>
+        </dialog>
+      </div>
+      :<div></div>
+    )
+  }
+
+  return (
+    <>
+    <div className="note">
+      <div className="content">
+        <textarea type="text" className="text" value={noteText} onChange={editNote} readOnly/>
+      </div>
+      <div className="actions">
+        <button className="edit" onClick={handleEditOpen}>Edit
+          <i className="fa-solid fa-pen-to-square" style={{ marginLeft: '8px' }}></i>
+        </button>
+        <button className="delete" onClick={handleDeleteOpen}>Delete
+        <i className="fa-solid fa-trash" style={{ marginLeft: '8px' }}></i>
+        </button>
+      </div>
+     </div>
+        {EditModal()}
+        {DeleteModal()}
       </>
   );
 }
