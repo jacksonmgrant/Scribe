8b1643f98cd3b6d98bbf5fdab8ef84ebd49daba6
junkshark8 <152639308+junkshark8@users.noreply.github.com>
2024-04-15 15:50:16 -0500

    fix code style issues

--- ScribeBE/models/user_model.py
+++ ScribeBE/models/user_model.py
@@ -1,20 +1,23 @@
 from beanie import Document
 from pydantic import BaseModel, EmailStr
 
+
 class User(BaseModel):
     name: str
     email: EmailStr
     password: str
 
+
 class Login(BaseModel):
     email: EmailStr
     password: str
+
 
 class DbUser(Document):
     name: str
     email: EmailStr
     password: str
-    
+
     class Settings:
         name = "users"
         keep_nulls = False
