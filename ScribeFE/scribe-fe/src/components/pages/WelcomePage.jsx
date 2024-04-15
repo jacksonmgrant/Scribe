44505636060c2d3d54450f8a8f26562aa74dd249
junkshark8 <152639308+junkshark8@users.noreply.github.com>
2024-04-15 17:53:46 -0500

    style welcome page
    change layout, insert content from page 1 of project proposal

--- ScribeFE/scribe-fe/src/components/pages/WelcomePage.jsx
+++ ScribeFE/scribe-fe/src/components/pages/WelcomePage.jsx
@@ -7,15 +7,34 @@ const Welcomepage = () => {
     return(
         <div className="welcome-container">
             <h1>Welcome to Scribe</h1>
-            <h2>Overview</h2>
-            <div className="overview">
-                <p>Scribe is a web app that records audio transcriptions into notes.
-                The goal of this app is to implement an LLM to generate organized and easy-to-read 
-                notes from an audio recording. </p>
-            </div>
-            <h2>Demo</h2>
-            <div className="vidoeText">
-                <ReactPlayer url="https://www.youtube.com/watch?v=XJrT1xQZmKk" controls={true}/>
+            <div className="content">
+                <div className="videoText">
+                    <h2>Demo</h2>
+                    <div className='wrapper'>
+                        <ReactPlayer className='player' 
+                        width='100%'
+                        height='100%'
+                        url="https://www.youtube.com/watch?v=XJrT1xQZmKk" controls={true}/>
+                    </div>
+                </div>
+                <div className="overview">
+                    <h2>Overview</h2>
+                    <p>There are many situations where people need to take notes which 
+                        have barriers to note taking. A project manager needs to record meeting 
+                        minutes, but also run the meeting. A doctor needs to have a conversation 
+                        with their patient and get a detailed record of their responses. A 
+                        person with arthritis can’t write notes for an entire lecture. All of 
+                        these situations could be remedied with a personal Scribe. </p>
+                        <br></br>
+                        <h3 className='problem'>Problem</h3>
+                        <p>Barriers to taking notes arise when the would-be writer is occupied 
+                            by the situation at hand or otherwise impaired. They need a way to 
+                            take notes without doing it themselves.</p>
+                        <br></br>
+                        <h3 className='solution'>Solution</h3>
+                        <p>Scribe transcribes audio with the click of a button, and can generate 
+                            notes from a text using an AI model.</p>
+                </div>
             </div>
         </div>
     )
