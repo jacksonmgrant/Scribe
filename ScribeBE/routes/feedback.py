a3bb89090f73cd455a257e82d566fe45f1e51db9
junkshark8 <152639308+junkshark8@users.noreply.github.com>
2024-04-16 14:14:44 -0500

    refactor feedback router

--- ScribeBE/routes/feedback.py
+++ ScribeBE/routes/feedback.py
@@ -1,12 +1,17 @@
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException  # type: ignore
+from database.connection import Database
 from models.feedback_model import Feedback, DbFeedback
+
 
 feedback_router = APIRouter(tags=["Feedback"])
 
+feedback_database = Database(Feedback)
+
+
 @feedback_router.post("/", status_code=201)
-async def recieve_feedback(feedback:Feedback)-> dict| str:
+async def recieve_feedback(feedback: Feedback) -> dict | str:
     if not feedback.text or not feedback.rating:
         raise HTTPException(status_code=400, detail="Plz fill something")
-    new_feedback = DbFeedback(text=feedback.text,rating=feedback.rating)
-    await new_feedback.insert()
-    return {"detail":"successfully add new feedback"}
+    new_feedback = DbFeedback(text=feedback.text, rating=feedback.rating)
+    await feedback_database.save(new_feedback)
+    return {"detail": "successfully add new feedback"}
