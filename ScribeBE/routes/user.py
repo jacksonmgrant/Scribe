3de18f08e5018c89ef7c32cd0843d7161e3bc7ba
junkshark8 <152639308+junkshark8@users.noreply.github.com>
2024-04-16 14:15:00 -0500

    refactor user router

--- ScribeBE/routes/user.py
+++ ScribeBE/routes/user.py
@@ -1,28 +1,36 @@
-from fastapi import APIRouter, Depends, HTTPException, status
-from models.user_model import DbUser, User, Login, TokenResponse
 from auth.hash_password import HashPassword
 import auth.JWT_token as JWT_token
+from database.connection import Database
+from fastapi import APIRouter, HTTPException, status  # type: ignore
+from models.user_model import DbUser, User, Login, TokenResponse
 from datetime import timedelta
 
-hash_password = HashPassword()
+
 user_router = APIRouter(tags=["User"])
 
+user_database = Database(User)
+hash_password = HashPassword()
 
-@user_router.post("/", status_code=201)
-async def signup(user:User) -> dict:
+
+@user_router.post("/signup", status_code=201)
+async def signup(user: User) -> dict:
     # To check if name or email or password is blank
     if not user.name or not user.email or not user.password:
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="name or email or password can not be blank")
-    
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
+                            detail="name or email or password can not be blank"
+                            )
+
     existing_email = await DbUser.find_one(DbUser.email == user.email)
     if existing_email:
-        raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail="A user with this email already exists.") 
-    
+        raise HTTPException(status_code=status.HTTP_409_CONFLICT,
+                            detail="A user with this email already exists."
+                            )
+
     hashed_user_password = hash_password.create_hash(user.password)
-    new_user =  DbUser(name=user.name, email=user.email, password=hashed_user_password, role="user")
-    await new_user.insert()
-    
-    return {"msg": "successfully add new user"}
+    user.password = hashed_user_password
+    await user_database.save(user)
+    return {"message": "successfully add new user"}
+
 
 @user_router.post("/login", response_model=TokenResponse)
 async def login(user: Login):
@@ -30,27 +38,29 @@ async def login(user: Login):
 
     # for case email is not correct
     if not existing_user:
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Incorrect email or password, or user does not exist.")
-    
-    check_password = hash_password.verify_hash(user.password,existing_user.password)
-    
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
+                            detail="Incorrect email or password, or user does not exist.")
+
+    check_password = hash_password.verify_hash(user.password,
+                                               existing_user.password)
+
     if check_password:
         user_token = JWT_token.create_access_token(
             data={
-                "sub" : str(existing_user.id),
-                "email_id" : existing_user.email,
-                "password" : user.password
+                "sub": str(existing_user.id),
+                "email_id": existing_user.email,
+                "password": user.password
             },
             expires_delta=timedelta(minutes=JWT_token.ACCESS_TOKEN_EXPIRE_MINUTES)
         )
         return {"access_token": user_token, "token_type": "Bearer"}
-    
+
+    raise HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Invalid details passed."
+    )
+
+    # for case password is not correct
     # raise HTTPException(
-    #     status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid details passed."
-    # )
-
-    ## for case password is not correct
-    raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Incorrect email or password, or user does not exist.")
-
-
-
+    #     status_code=status.HTTP_400_BAD_REQUEST,
+    #     detail="Incorrect email or password, or user does not exist.")
