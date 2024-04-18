468a5c0546fc7b90ad5e79f6d2f33d6bc8841af7
junkshark8 <152639308+junkshark8@users.noreply.github.com>
2024-04-18 09:59:44 -0500

    update main.py
    update database connection, add logger

--- ScribeBE/main.py
+++ ScribeBE/main.py
@@ -1,34 +1,42 @@
 from contextlib import asynccontextmanager
 from functools import lru_cache
-from fastapi import FastAPI, HTTPException, UploadFile, APIRouter
-from fastapi.middleware.cors import CORSMiddleware
-from fastapi.responses import RedirectResponse
-from settings import Settings
-from models.note_model import DbNote
-import routes.note as note
-import routes.user as user
-import routes.feedback as feedback
-import transcriber
-from database.database import init_db
+import logging
+from fastapi import FastAPI, HTTPException, UploadFile, APIRouter  # type: ignore
+from fastapi.middleware.cors import CORSMiddleware  # type: ignore
+from fastapi.responses import RedirectResponse  # type: ignore
 
-async def start_db():
-    await init_db()
+from database.connection import Settings
+from logging_setup import setup_logging
+from models.note_model import DbNote
+from routes.note import note_router
+from routes.user import user_router
+from routes.feedback import feedback_router
+import transcriber
+
+setup_logging()
+logger = logging.getLogger(__name__)
+
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     # on startup event
-    #logger.info("Application starts up...")
-    get_settings()
-    await start_db()
+    logger.info("Application starts up...")
+    await get_settings().initialize_database()
     yield
     # on shutdown event
     ...
+
+app = FastAPI(title="Scribe", version="0.1.0", lifespan=lifespan)
+
 
 @lru_cache
 def get_settings():
     return Settings()
 
-app = FastAPI(title="Scribe", version="0.1.0", lifespan=lifespan)
+
+# register origins
+
+origins = ["*"]
 
 app.add_middleware(
     CORSMiddleware,
@@ -42,27 +50,33 @@ app.add_middleware(
 root_router = APIRouter()
 transcription_router = APIRouter()
 
+
 @root_router.get("/")
 async def root():
     return RedirectResponse(url="/notes/")
 
 allowed_content_types = ["audio/wav", "audio/x-wav"]
+
+
 @transcription_router.post("/", status_code=201)
 async def transcribe(file: UploadFile) -> dict:
     if file.content_type not in allowed_content_types:
         raise HTTPException(status_code=400, detail="File must be a .wav file")
-    
+
     speech = transcriber.transcribe(file)
 
     if "ResultReason" in speech:
         raise HTTPException(status_code=500, detail=speech)
-    
+
     new_note = DbNote(text=speech, hasRecording=True)
     await new_note.insert()
-    return {"transcribed" : speech}
+    return {"transcribed": speech}
+
+# Register routes
 
 app.include_router(root_router, tags=["Root"])
-app.include_router(transcription_router, prefix="/transcribe", tags=["Transcription"])
-app.include_router(note.note_router, prefix="/notes")
-app.include_router(user.user_router, prefix="/users")
-app.include_router(feedback.feedback_router, prefix="/feedback")
+app.include_router(transcription_router, prefix="/transcribe",
+                   tags=["Transcription"])
+app.include_router(note_router, prefix="/notes")
+app.include_router(user_router, prefix="/users")
+app.include_router(feedback_router, prefix="/feedback")
