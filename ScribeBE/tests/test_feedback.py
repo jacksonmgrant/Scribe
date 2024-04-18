0f93da548f68f411f5d94c88150970dd365118fb
Nithi Pipatkittikul <fordpipatkitkul@gmail.com>
2024-04-18 18:28:58 -0500

    test_feedback_routes
    not sure

--- ScribeBE/tests/test_feedback.py
+++ ScribeBE/tests/test_feedback.py
@@ -1,8 +1,7 @@
 import pytest
-from auth.JWT_token import create_access_token
 from bson.objectid import ObjectId
 from httpx import AsyncClient
-from routes.feedback import recieve_feedback
+from auth.JWT_token import create_access_token
 
 @pytest.fixture
 async def access_token() -> str:
@@ -11,3 +10,22 @@ async def access_token() -> str:
                 "email_id": 'a@gmail.com',
                 "password": "$2b$12$y6yvtbPyUXZmhej1cKD7hewcIGKWbV96sJIhSvjONpy4Q0sHMzVi2"
             })
+
+@pytest.mark.anyio
+async def test_recieve_feedback(default_client: AsyncClient, access_token: str) -> None:
+    Headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {access_token}"
+    }
+
+    Payload = {
+        "text" : "good",
+        "rating" : 3
+    }
+
+    test_response = {"detail":"successfully add new feedback"}
+
+    response = await default_client.post("/feedback/",json=Payload,headers=Headers)
+
+    assert response.status_code == 201
+    assert response.json() == test_response
