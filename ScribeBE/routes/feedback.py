bd7c219d28fac33315037475e853754763a6cb72
Nithi Pipatkittikul <fordpipatkitkul@gmail.com>
2024-04-18 15:23:48 -0500

    fix feedback
    in the log we see email instead of Mongodb's id

--- ScribeBE/routes/feedback.py
+++ ScribeBE/routes/feedback.py
@@ -12,12 +12,11 @@ feedback_database = Database(Feedback)
 
 
 @feedback_router.post("/", status_code=201)
-async def recieve_feedback(feedback: Feedback,
-                           user: str = Depends(authenticate)) -> dict | str:
+async def recieve_feedback(feedback: Feedback, user: str = Depends(authenticate)) -> dict | str:
     if not feedback.text or not feedback.rating:
         logger.warning("Fields cannot be blank")
         raise HTTPException(status_code=400, detail="Plz fill something")
     new_feedback = DbFeedback(text=feedback.text, rating=feedback.rating)
     await feedback_database.save(new_feedback)
-    logger.info(f"New feedback from {user.email} created")
+    logger.info(f"New feedback from {user["email_id"]} created")
     return {"detail": "successfully add new feedback"}
