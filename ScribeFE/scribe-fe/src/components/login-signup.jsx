5643f69c12b4ef5f586086cbcfee776a0aa401b4
junkshark8 <152639308+junkshark8@users.noreply.github.com>
2024-04-24 13:20:57 -0500

    fix #33
    add hooks for empty email and password fields, fix clearInputFields bug

--- ScribeFE/scribe-fe/src/components/login-signup.jsx
+++ ScribeFE/scribe-fe/src/components/login-signup.jsx
@@ -3,9 +3,26 @@ import "../styles/LoginSignup.css"
 import {Link,useNavigate} from 'react-router-dom';
 import apiService from '../services/apiService';
 
-const LoginSignup = ({signin,signout,email,setEmail,password,setPassword,clearLoginInput}) => {
+const LoginSignup = ({signin,signout,email,setEmail,password,setPassword}) => {
+    const [emailEmpty, setEmailEmpty] = useState(true);
+    const [passwordEmpty, setPasswordEmpty] = useState(true);
+    
     const [cannotLogin,setCannotLogin] = useState(false);
     const navigate = useNavigate();
+
+    function clearLoginInput() {
+        document.getElementsByTagName('input').value = '';
+    }
+
+    function handleEmailInput(event) {
+        setEmail(event.target.value);
+        setEmailEmpty(false);
+    }
+
+    function handlePasswordInput(event) {
+        setPassword(event.target.value);
+        setPasswordEmpty(false);
+    }
     
     return(
         <form method='GET'>
@@ -21,23 +38,34 @@ const LoginSignup = ({signin,signout,email,setEmail,password,setPassword,clearLo
                         name="userid" 
                         placeholder='Email'
                         required
-                        onChange={(event) => setEmail(event.target.value)}
+                        onChange={(event) => handleEmailInput(event)}
                         />
                     </div>
+                    {emailEmpty && <p id="emailMsg" style={{ color: 'var(--danger)', display: 'none' }}>Email cannot be blank</p>}
                     <div className='input'>
                         <i className="fa-solid fa-lock"></i>
                         <input type='password' 
                         name="password" 
                         placeholder='Password'
                         required
-                        onChange={(event) => setPassword(event.target.value)}
+                        onChange={(event) => handlePasswordInput(event)}
                         />
                     </div>
-                    {cannotLogin && <p style={{ color: 'var(--danger)' }}>Incorrect email or password</p>}
+                    {passwordEmpty && <p id="passwordMsg" style={{ color: 'var(--danger)', display: 'none' }}>Password cannot be blank</p>}
+                    {cannotLogin && <p id="errMsg" style={{ color: 'var(--danger)', display: 'none' }}>Incorrect email or password</p>}
                 </div>
                 <div className='submit-container'>
                     <Link className="submit" 
                         onClick={async () => {
+                            if (emailEmpty) {
+                                document.getElementById('emailMsg').style.display = 'block';
+                            }
+                            if (passwordEmpty) {
+                                document.getElementById('passwordMsg').style.display = 'block';
+                            }
+                            else if (cannotLogin) {
+                                document.getElementById('errMsg').style.display = 'block';
+                            }
                             await apiService.checkUser(email,password,setCannotLogin,navigate,signin,signout)
                             clearLoginInput()
                         }}
