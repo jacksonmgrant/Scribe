6dc82641d8e4169b18742326f1a4de6f0fb9b6b5
junkshark8 <152639308+junkshark8@users.noreply.github.com>
2024-04-24 17:46:10 -0500

    responsive nav

--- ScribeFE/scribe-fe/src/Layout.jsx
+++ ScribeFE/scribe-fe/src/Layout.jsx
@@ -2,6 +2,16 @@ import { Outlet, Link } from "react-router-dom";
 import Logo from "./components/Assets/logo-bw.png";
 import "./styles/layout.css";
 
+// responsive hamburger menu
+function collapseMenu() {
+    var x = document.getElementById("main-nav");
+    if (x.className === "main-nav") {
+        x.className += " responsive";
+    } else {
+        x.className = "main-nav";
+    }
+}
+
 const Layout = ({isSignin,signout}) => {
 
     return (
@@ -10,42 +20,57 @@ const Layout = ({isSignin,signout}) => {
           <div>
             { isSignin === false
             ?(
-              <div className='zone gradient'>
-                <div className='main-nav'>
+              <div className='main-nav' id="main-nav">
+                <ul>
                   <li>
-                      <Link to="/" style={{ fontWeight: '700', display: 'flex', alignItems: 'center' }}>
+                      <Link to="/" style={{ fontWeight: '700', display: 'flex', alignItems: 'center' }}
+                      onClick={collapseMenu}>
                       <img alt="Scribe Logo" src={Logo} style={{ height: '1.75rem', marginRight: '0.5rem' }}></img>
                       <span style={{ paddingBottom: '3px' }}>Scribe</span></Link>
                   </li>
                   <li className="push">
-                      <Link to="/loginSignupPage">Log In
+                      <Link to="/loginSignupPage"
+                      onClick={collapseMenu}>Log In
                       <i className="fa-solid fa-right-to-bracket" style={{ marginLeft: '8px' }}></i>
                       </Link>
                   </li>
-                </div>
+                </ul>
+                {/* hamburger icon for small screens */}
+                <Link href="javascript:void(0);" class="icon" onClick={collapseMenu}>
+                    <i class="fa fa-bars"></i>
+                </Link>
               </div>
               ) 
             : (
-              <div className='zone gradient'>
-                <div className='main-nav'>
-                  <li>
-                      <Link to="/userpage" style={{ fontWeight: '700', display: 'flex', alignItems: 'center' }}>
-                      <img alt="Scribe Logo" src={Logo} style={{ height: '1.75rem', marginRight: '0.5rem' }}></img>
-                      <span style={{ paddingBottom: '2px' }}>Scribe</span></Link>
-                  </li>
-                  <li>
-                      <Link id="aboutus" to="/aboutus">About us</Link>
-                  </li>
-                  <li>
-                      <Link id="formsubmission" to="/formsubmission">User feedback</Link>
-                  </li>
-                  <li className="push">
-                      <Link to="/loginSignupPage"  onClick={signout}>Log out
-                      <i className="fa-solid fa-right-to-bracket" style={{ marginLeft: '8px' }}></i>
-                      </Link>
-                  </li>
+              <nav>
+                <div className='main-nav' id="main-nav">
+                  <ul>
+                    <li>
+                        <Link to="/userpage" style={{ fontWeight: '700', display: 'flex', alignItems: 'center' }}
+                        onClick={collapseMenu}>
+                        <img alt="Scribe Logo" src={Logo} style={{ height: '1.75rem', marginRight: '0.5rem' }}></img>
+                        <span style={{ paddingBottom: '2px' }}>Scribe</span></Link>
+                    </li>
+                    <li>
+                        <Link id="aboutus" to="/aboutus"
+                        onClick={collapseMenu}>About us</Link>
+                    </li>
+                    <li>
+                        <Link id="formsubmission" to="/formsubmission"
+                        onClick={collapseMenu}>User feedback</Link>
+                    </li>
+                    <li className="push">
+                        <Link to="/loginSignupPage"  onClick={signout}>Log out
+                        <i className="fa-solid fa-right-to-bracket" style={{ marginLeft: '8px' }}></i>
+                        </Link>
+                    </li>
+                  </ul>
+                 {/* hamburger icon for small screens */}
+                  <Link href="javascript:void(0);" class="icon" onClick={collapseMenu}>
+                      <i class="fa fa-bars"></i>
+                  </Link>
                 </div>
-              </div>
+              </nav>
               )
             }
           </div>
