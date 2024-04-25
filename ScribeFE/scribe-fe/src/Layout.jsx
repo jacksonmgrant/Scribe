e2f1a38bbf9de21f228af3486c5c5f651d5acf05
junkshark8 <152639308+junkshark8@users.noreply.github.com>
2024-04-25 17:42:07 -0500

    fix responsive nav issues

--- ScribeFE/scribe-fe/src/Layout.jsx
+++ ScribeFE/scribe-fe/src/Layout.jsx
@@ -2,17 +2,23 @@ import { Outlet, Link } from "react-router-dom";
 import Logo from "./components/Assets/logo-bw.png";
 import "./styles/layout.css";
 
-// responsive hamburger menu
-function collapseMenu() {
+const Layout = ({isSignin,signout}) => {
+  // responsive hamburger menu
+  function toggleMenu() {
     var x = document.getElementById("main-nav");
     if (x.className === "main-nav") {
-        x.className += " responsive";
-    } else {
-        x.className = "main-nav";
+      x.classList.add("responsive");
     }
-}
+    else {
+      x.classList.remove("responsive");
+    }
+  }
 
-const Layout = ({isSignin,signout}) => {
+  function logOut() {
+    toggleMenu();
+    signout();
+  }
+
 
     return (
       <div>
@@ -24,53 +30,50 @@ const Layout = ({isSignin,signout}) => {
                 <ul>
                   <li>
                       <Link to="/" style={{ fontWeight: '700', display: 'flex', alignItems: 'center' }}
-                      onClick={collapseMenu}>
+                      onClick={toggleMenu}>
                       <img alt="Scribe Logo" src={Logo} style={{ height: '1.75rem', marginRight: '0.5rem' }}></img>
                       <span style={{ paddingBottom: '3px' }}>Scribe</span></Link>
                   </li>
                   <li className="push">
-                      <Link to="/loginSignupPage"
-                      onClick={collapseMenu}>Log In
+                      <Link to="/loginSignupPage" onClick={toggleMenu}>Log In
                       <i className="fa-solid fa-right-to-bracket" style={{ marginLeft: '8px' }}></i>
                       </Link>
                   </li>
                 </ul>
                 {/* hamburger icon for small screens */}
-                <Link href="javascript:void(0);" class="icon" onClick={collapseMenu}>
-                    <i class="fa fa-bars"></i>
-                </Link>
+                <a href="javascript:void(0);" className="icon" onClick={toggleMenu}>
+                    <i className="fa fa-bars"></i>
+                </a>
               </div>
               ) 
             : (
-              <nav>
-                <div className='main-nav' id="main-nav">
-                  <ul>
-                    <li>
-                        <Link to="/userpage" style={{ fontWeight: '700', display: 'flex', alignItems: 'center' }}
-                        onClick={collapseMenu}>
-                        <img alt="Scribe Logo" src={Logo} style={{ height: '1.75rem', marginRight: '0.5rem' }}></img>
-                        <span style={{ paddingBottom: '2px' }}>Scribe</span></Link>
-                    </li>
-                    <li>
-                        <Link id="aboutus" to="/aboutus"
-                        onClick={collapseMenu}>About us</Link>
-                    </li>
-                    <li>
-                        <Link id="formsubmission" to="/formsubmission"
-                        onClick={collapseMenu}>User feedback</Link>
-                    </li>
-                    <li className="push">
-                        <Link to="/loginSignupPage"  onClick={signout}>Log out
-                        <i className="fa-solid fa-right-to-bracket" style={{ marginLeft: '8px' }}></i>
-                        </Link>
-                    </li>
-                  </ul>
-                 {/* hamburger icon for small screens */}
-                  <Link href="javascript:void(0);" class="icon" onClick={collapseMenu}>
-                      <i class="fa fa-bars"></i>
-                  </Link>
-                </div>
-              </nav>
+              <div className='main-nav' id="main-nav">
+                <ul>
+                  <li>
+                      <Link to="/userpage" style={{ fontWeight: '700', display: 'flex', alignItems: 'center' }}
+                      onClick={toggleMenu}>
+                      <img alt="Scribe Logo" src={Logo} style={{ height: '1.75rem', marginRight: '0.5rem' }}></img>
+                      <span style={{ paddingBottom: '2px' }}>Scribe</span></Link>
+                  </li>
+                  <li>
+                      <Link id="aboutus" to="/aboutus"
+                      onClick={toggleMenu}>About us</Link>
+                  </li>
+                  <li>
+                      <Link id="formsubmission" to="/formsubmission"
+                      onClick={toggleMenu}>User feedback</Link>
+                  </li>
+                  <li className="push">
+                      <Link to="/loginSignupPage"  onClick={logOut}>Log out
+                      <i className="fa-solid fa-right-to-bracket" style={{ marginLeft: '8px' }}></i>
+                      </Link>
+                  </li>
+                </ul>
+              {/* hamburger icon for small screens */}
+                <a href="javascript:void(0);" className="icon" onClick={toggleMenu}>
+                    <i className="fa fa-bars"></i>
+                </a>
+              </div>
               )
             }
           </div>
